05345792117d88cabffacf3d948c1909da16a6b7
ajiinisti <ajiinisti@gmail.com>
2023-06-03 22:27:54 +0700

    [FIX] Sidebar Styling

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,27 +1,34 @@
-import TalentConnectLogo from "./icons/TalentConnectLogo.svg"
-import DefaultProfileIcon from "./icons/ProfilePictureIcon.svg"
-import ProgramIcon from "./icons/darhboard_alt.svg"
-import SettingsIcon from "./icons/Frame 3.svg"
-import UserManagementIcon from "./icons/Frame 2025.svg"
-import EvaluationScoringIcon from "./icons/Frame 2026.svg"
-import WhiteProgramIcon from "./icons/WhiteProgramIcon.svg"
-import WhiteSettingsIcon from "./icons/WhiteSettingsIcon.svg"
-import WhiteUserManagementIcon from "./icons/WhiteUserManagementIcon.svg"
-import WhiteEvaluationScoringIcon from "./icons/WhiteEvaluationScoringIcon.svg"
-import LoginLogo from './icons/LoginLogo.svg'
+import TalentConnectLogo from './icons/TalentConnectLogo.svg'
 import GoogleLoginLogo from './icons/GoogleLoginLogo.svg'
+import LoginLogo from './icons/LoginLogo.svg'
+import LogoutIcon from './icons/LogoutIcon.svg'
+import DefaultProfileIcon from './icons/ProfilePictureIcon.svg'
+import EvaluationIconActive from './icons/EvaluationIconActive.svg'
+import EvaluationIconNonActive from './icons/EvaluationIconNonActive.svg'
+import EvaluationScoringIconActive from './icons/EvaluationScoringIconActive.svg'
+import EvaluationScoringIconNonActive from './icons/EvaluationScoringIconNonActive.svg'
+import ProfileSettingsIconActive from './icons/ProfileSettingsIconActive.svg'
+import ProfileSettingsIconNonActive from './icons/ProfileSettingsIconNonActive.svg'
+import ProgramIconActive from './icons/ProgramIconActive.svg'
+import ProgramIconNonActive from './icons/ProgramIconNonActive.svg'
+import UserManagementIconActive from './icons/UserManagementIconActive.svg'
+import UserManagementIconNonActive from './icons/UserManagementIconNonActive.svg'
+
 
 export {
     TalentConnectLogo,
     DefaultProfileIcon,
-    ProgramIcon,
-    SettingsIcon,
-    UserManagementIcon,
-    EvaluationScoringIcon,
-    WhiteProgramIcon,
-    WhiteEvaluationScoringIcon,
-    WhiteUserManagementIcon,
-    WhiteSettingsIcon,
     LoginLogo,
-    GoogleLoginLogo
+    GoogleLoginLogo,
+    LogoutIcon,
+    EvaluationIconActive,
+    EvaluationIconNonActive,
+    EvaluationScoringIconActive,
+    EvaluationScoringIconNonActive,
+    ProfileSettingsIconActive,
+    ProfileSettingsIconNonActive,
+    ProgramIconActive,
+    ProgramIconNonActive,
+    UserManagementIconActive,
+    UserManagementIconNonActive
 }
