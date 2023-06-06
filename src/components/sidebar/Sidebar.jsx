d26a22ddf945342ac3b8c7c308648c23287cf19d
ajiinisti <ajiinisti@gmail.com>
2023-06-06 16:17:07 +0700

    [FIX]Sidebar reponsitivity

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -24,58 +24,58 @@ const Sidebar = () => {
     }, [location]);
 
     return (
-        <div className="sidebar d-flex flex-column align-items-center align-items-sm-start px-5 pt-5 text-secondary min-vh-100">
-        <div>
-            <img src={TalentConnectLogo} alt="Logo" className="icon-navbar" />
-        </div>
-        <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start font-bold" id="menu">
-            <NavLink to="/program" activeclassname="active" >
-                <div className="mt-4 navbar-div-item" >
-                    <li className="nav-item">
-                        <img src={activeNavLink.includes('/program') ? ProgramIconActive : ProgramIconNonActive}
-                            className="icon-navbar" alt="Program Icon" />
-                    </li>
-                </div>
-            </NavLink>
-            <NavLink to="/user-management" activeclassname="active" >
-                <div className="mt-1 navbar-div-item" >
-                    <li className="nav-item">
-                        <img src={activeNavLink.includes('/user-management') ? UserManagementIconNonActive : UserManagementIconActive}
-                        className="icon-navbar" alt="User Management Icon" />
-                    </li>
-                </div>
-            </NavLink>
-            <NavLink to="/evaluation-scoring" activeclassname="active">
-                <div className="mt-1 navbar-div-item" >
-                    <li className="nav-item">
-                        <img src={activeNavLink.includes('/evaluation-scoring') ? EvaluationScoringIconActive : EvaluationScoringIconNonActive}
-                        className="icon-navbar" alt="Evaluation Scoring Icon" />
-                    </li>
-                </div> 
-            </NavLink>
-            <NavLink to="/evaluations/id" activeclassname="active">
-                <div className="mt-1 navbar-div-item" >
-                    <li className="nav-item">
-                        <img src={activeNavLink.includes('/evaluations') ? EvaluationIconActive : EvaluationIconNonActive}
-                        className="icon-navbar" alt="Evaluation Scoring Icon" />
-                    </li>
-                </div> 
-            </NavLink>
-            <NavLink to="/settings" activeclassname="active">
-                <div className="mt-1 navbar-div-item" >
-                    <li className="nav-item">
-                        <img src={activeNavLink.includes('/settings') ? ProfileSettingsIconActive : ProfileSettingsIconNonActive}
-                        className="icon-navbar" alt="Settings Icon" />
-                    </li>
-                </div>
-            </NavLink>
-        </ul>
-        <hr />
-        <div className="dropdown pb-4">
-            <button className="btn" style={{backgroundColor: '#F1FAFB', border:'#1FAFB', outline: 'none', cursor:'pointer'}}>
-                <img src={LogoutIcon} alt="Profile Icon" className="icon-navbar" />
-            </button>
-        </div>
+        <div className="sidebar d-flex flex-column align-items-center align-items-sm-start px-3 px-sm-5 pt-5 text-secondary min-vh-100">
+            <div>
+                <img src={TalentConnectLogo} alt="Logo"/>
+            </div>
+            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start font-bold" id="menu">
+                <NavLink to="/program" activeclassname="active" >
+                    <div className="mt-4 navbar-div-item" >
+                        <li className="nav-item">
+                            <img src={activeNavLink.includes('/program') ? ProgramIconActive : ProgramIconNonActive}
+                                className="icon-navbar" alt="Program Icon" />
+                        </li>
+                    </div>
+                </NavLink>
+                <NavLink to="/user-management" activeclassname="active" >
+                    <div className="mt-1 navbar-div-item" >
+                        <li className="nav-item">
+                            <img src={activeNavLink.includes('/user-management') ? UserManagementIconNonActive : UserManagementIconActive}
+                            className="icon-navbar" alt="User Management Icon" />
+                        </li>
+                    </div>
+                </NavLink>
+                <NavLink to="/evaluation-scoring" activeclassname="active">
+                    <div className="mt-1 navbar-div-item" >
+                        <li className="nav-item">
+                            <img src={activeNavLink.includes('/evaluation-scoring') ? EvaluationScoringIconActive : EvaluationScoringIconNonActive}
+                            className="icon-navbar" alt="Evaluation Scoring Icon" />
+                        </li>
+                    </div> 
+                </NavLink>
+                <NavLink to="/evaluations/id" activeclassname="active">
+                    <div className="mt-1 navbar-div-item" >
+                        <li className="nav-item">
+                            <img src={activeNavLink.includes('/evaluations') ? EvaluationIconActive : EvaluationIconNonActive}
+                            className="icon-navbar" alt="Evaluation Scoring Icon" />
+                        </li>
+                    </div> 
+                </NavLink>
+                <NavLink to="/settings" activeclassname="active">
+                    <div className="mt-1 navbar-div-item" >
+                        <li className="nav-item">
+                            <img src={activeNavLink.includes('/settings') ? ProfileSettingsIconActive : ProfileSettingsIconNonActive}
+                            className="icon-navbar" alt="Settings Icon" />
+                        </li>
+                    </div>
+                </NavLink>
+            </ul>
+            <hr />
+            <div className="dropdown pb-4">
+                <button className="btn" style={{backgroundColor: '#F1FAFB', border:'#1FAFB', outline: 'none', cursor:'pointer'}}>
+                    <img src={LogoutIcon} alt="Profile Icon"/>
+                </button>
+            </div>
         </div>
   );
 }
