10147ca53c5f28c7c8d511037219bbb1b99c39d7
alwinihza <alwinihza@gmail.com>
2023-06-07 14:57:05 +0700

    [ADD] fetching qustion, program and evaluation

--- src/pages/activityDetail/ActivityDetail.jsx
+++ src/pages/activityDetail/ActivityDetail.jsx
@@ -1,8 +1,13 @@
 // import { useNavigate } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { DefaultProfileIcon } from "../../assets"
 import { BsArrowLeft } from "react-icons/bs"
+import { useEffect, useState } from "react"
+import axiosInstance from "../../services/axios-client"
 
 const ActivityDetail = () => {
+    const params = useParams()
+    const [activity, setActivity] = useState({})
     // const navigate = useNavigate()
     // const buttonStyle = {
     //     borderRadius : '5px'
@@ -13,21 +18,36 @@ const ActivityDetail = () => {
     //     bot : '1.5rem'
     // }
 
+    useEffect(()=>{
+        const getDetail = async () => {
+            try {
+                let res = await axiosInstance.get("activities/"+params.id)
+                if (res.status === 200) {
+                    setActivity(res.data.data)
+                    console.log(new Date(res.data.data.StartDate).toDateString())
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getDetail()
+    }, [])
+
     return(
         <div className="container py-3 px-5">
             <h1 className="mt-2"><b>SMM ITDP Batch 3</b></h1>
             <hr/>
             <div className="row">
                 <div className="col-md-9 mr-3">      
-                    <h4 className="mt-4 mb-4"><BsArrowLeft/>   KICK OFF ITDP BATCH 3</h4>
+                    <h4 className="mt-4 mb-4"><BsArrowLeft/>   {activity?.Name}</h4>
                     <div className="mt-4 px-4 py-4" style={{ border: '0.5px solid #d3d3d3', borderRadius:'10px'}}>
                         <div className="mb-4" style={{ display: 'flex', flexDirection: 'column'}}>
                             <label style={{ display: 'inline-block'}}>Link</label>
-                            <b><span>http://zoom.com</span></b>
+                            <b><span>{activity?.Link}</span></b>
                         </div>
                         <div className="mb-4" style={{ display: 'flex', flexDirection: 'column'}}>
                             <label style={{ display: 'inline-block'}}>Date</label>
-                            <b><span>12/12/2023</span></b>
+                            <b><span>{new Date(activity?.StartDate).toLocaleString('id-ID')} - {new Date(activity?.EndDate).toLocaleString('id-ID')}</span></b>
                         </div>
                         <div className="mb-4" style={{ display: 'flex', flexDirection: 'column'}}>
                             <label style={{ display: 'inline-block'}}>Participants</label>
@@ -35,7 +55,7 @@ const ActivityDetail = () => {
                         </div>
                         <div className="mb-4" style={{ display: 'flex', flexDirection: 'column'}}>
                             <label style={{ display: 'inline-block'}}>Description</label>
-                            <b><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magni voluptatum quam sed pariatur labore alias dolores quod natus perspiciatis iure nobis doloribus quasi magnam et culpa est, animi voluptatibus.</span></b>
+                            <b><span>{activity?.Description}</span></b>
                         </div>
                     </div>
                 </div>
