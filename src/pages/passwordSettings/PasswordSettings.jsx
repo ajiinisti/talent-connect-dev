175d708c55241cdd39f9f90c24d2d50c95baa7cb
ajiinisti <ajiinisti@gmail.com>
2023-07-06 16:16:08 +0700

    [ADD] Validation on All Form

--- src/pages/passwordSettings/PasswordSettings.jsx
+++ src/pages/passwordSettings/PasswordSettings.jsx
@@ -66,59 +66,61 @@ const PasswordSettings = () => {
         <div className="container py-5 px-5">
             <h2><b>Change Password</b></h2>
             <div className="mt-4">
-                <div className="mt-3">  
-                    <label htmlFor="currentPassword" className="form-label">Current Password</label>
-                    <input 
-                        required
-                        type="password" 
-                        name="currentPassword" 
-                        className="form-control" 
-                        id="currentPassword" 
-                        placeholder="Enter your current password"
-                        onChange={handleInputChange}
-                    />
-                </div>
+                <form onSubmit={() => {
+                    changePassword(passwordData.currentPassword, passwordData.confirmPassword)
+                }}>
+                    <div className="mt-3">  
+                        <label htmlFor="currentPassword" className="form-label">Current Password</label>
+                        <input 
+                            required
+                            type="password" 
+                            name="currentPassword" 
+                            className="form-control" 
+                            id="currentPassword" 
+                            placeholder="Enter your current password"
+                            onChange={handleInputChange}
+                        />
+                    </div>
 
-                <div className="mt-3">
-                    <label htmlFor="newPassword" className="form-label">New Password</label>
-                    <input 
-                        required
-                        type="password" 
-                        name="newPassword" 
-                        className="form-control" 
-                        id="newPassword" 
-                        placeholder="Enter your new password"
-                        onChange={handleInputChange}
-                    />
-                </div>
+                    <div className="mt-3">
+                        <label htmlFor="newPassword" className="form-label">New Password</label>
+                        <input 
+                            required
+                            type="password" 
+                            name="newPassword" 
+                            className="form-control" 
+                            id="newPassword" 
+                            placeholder="Enter your new password"
+                            onChange={handleInputChange}
+                        />
+                    </div>
 
-                <div className="mt-3">
-                    <label htmlFor="confirmPassword" className="form-label">Confirm New Password</label>
-                    <input 
-                        required
-                        type="password" 
-                        name="confirmPassword" 
-                        className="form-control" 
-                        id="confirmPassword" 
-                        placeholder="Confirm you new password"
-                        onChange={handleInputChange}
-                    />
-                </div>
+                    <div className="mt-3">
+                        <label htmlFor="confirmPassword" className="form-label">Confirm New Password</label>
+                        <input 
+                            required
+                            type="password" 
+                            name="confirmPassword" 
+                            className="form-control" 
+                            id="confirmPassword" 
+                            placeholder="Confirm you new password"
+                            onChange={handleInputChange}
+                        />
+                    </div>
 
-                {
-                    passwordData.errorMessage ?
-                    <p style={{color: "red"}}>{passwordData.errorMessage}</p> : <></>
-                }
+                    {
+                        passwordData.errorMessage ?
+                        <p style={{color: "red"}}>{passwordData.errorMessage}</p> : <></>
+                    }
 
-                <div className="mt-3">
-                    <Button
-                        title={"Save Changes"}
-                        navigate={() => {
-                            changePassword(passwordData.currentPassword, passwordData.confirmPassword)
-                        }}
-                    />
-                    <CancelButton/>
-                </div>
+                    <div className="mt-3">
+                        <Button
+                            title={"Save Changes"}
+                            navigate={() => (0)}
+                        />
+                        <CancelButton/>
+                    </div>    
+                </form>
             </div>
         </div>
     )
